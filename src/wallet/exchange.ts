be723a3d4e4307d5ac436278d2099f38c2cb6b94
mgthomas99 <m.g.thomas99@gmail.com>
2018-11-05 00:08:25 +0000

    doc, events, removed unused methods

--- src/wallet/exchange.ts
+++ src/wallet/exchange.ts
@@ -3,72 +3,96 @@ import { EventEmitter } from "events";
 import { StrictEventEmitter } from "../../typings/initiative-strict-event-emitter";
 import { Transaction } from "./transaction";
 import { Wallet } from "./wallet";
+import { RPC } from "../rpc/rpc";
 
 export class Exchange {
 
   public readonly events: StrictEventEmitter<{
-    "tx.queued": (tx: Transaction) => any;
-    "tx.processed": (tx: Transaction) => any;
+    "tx.failed.circular": (tx: Transaction) => void;
+    "tx.failed.insufficientfunds": (tx: Transaction) => void;
+    "tx.failed.noreceiver": (tx: Transaction) => void;
+    "tx.failed.nosender": (tx: Transaction) => void;
+    "tx.processed": (tx: Transaction) => void;
   }>;
 
-  public queue: Array<Transaction>;
-  public users: Array<Wallet>;
+  public wallets: Array<Wallet>;
 
   public constructor() {
     this.events = new EventEmitter();
-
-    this.users = [];
-    this.queue = [];
+    this.wallets = [];
   }
 
-  public processAll()
-  : (void) {
-    while (this.queue.length > 0) {
-      this.processNext();
+  public async save(rpc: RPC)
+  : (Promise<void>) {
+    for (const wallet of this.wallets) {
+      await rpc.saveWallet(wallet);
     }
   }
 
-  public processNext()
-  : (void) {
-    if (this.queue.length === 0) return;
-    const tx = this.queue[0];
-    return this.process(tx);
+  public async load(rpc: RPC)
+  : (Promise<void>) {
+    const wallets = await rpc.loadWallets();
+    this.wallets = wallets;
   }
 
+  /**
+   * Apply the specified transaction to this exchange's accounts.
+   *
+   * @param   {Transaction} tx
+   *          The transaction.
+   */
   public process(tx: Transaction)
   : (void) {
-    const alice = this.users.find(x => x.address === tx.from);
-    const bob = this.users.find(x => x.address === tx.to);
+    const alice = this.wallets.find(x => x.address === tx.from);
+    const bob = this.wallets.find(x => x.address === tx.to);
 
-    if (typeof alice === "undefined")
+    if (typeof alice === "undefined") {
+      this.events.emit("tx.failed.nosender", tx);
       throw new Error("Sender does not exist");
-    if (typeof bob === "undefined")
+    } else if (typeof bob === "undefined") {
+      this.events.emit("tx.failed.noreceiver", tx);
       throw new Error("Receiver does not exist");
+    } else if (alice === bob) {
+      this.events.emit("tx.failed.circular", tx);
+      return;
+    }
+
+    if (alice.balance < tx.amount) {
+      this.events.emit("tx.failed.insufficientfunds", tx);
+      throw new Error("Sender has insufficient funds");
+    }
 
     alice.balance -= tx.amount;
     bob.balance += tx.amount;
+    this.events.emit("tx.processed", tx);
   }
 
-  public send(a: Wallet | string, b: Wallet | string, amount: number)
+  /**
+   *
+   * @param   {Wallet | string} alice
+   *          The sender.
+   * @param   {Wallet | string} bob
+   *          The receiver.
+   * @param   {number}  amount
+   *          The amount to send.
+   */
+  public send(alice: Wallet | string, bob: Wallet | string, amount: number)
   : (void) {
-    if (typeof a === "string") {
-      const a_wallet = this.users.find(x => x.address === a);
-      return this.send(a_wallet, b, amount);
+    if (typeof alice === "string") {
+      const a_wallet = this.wallets.find(x => x.address === alice);
+      return this.send(a_wallet, bob, amount);
+    } else if (typeof bob === "string") {
+      const b_wallet = this.wallets.find(x => x.address === bob);
+      return this.send(alice, b_wallet, amount);
+    } else if (alice === bob) {
+      return;
     }
-    if (typeof b === "string") {
-      const b_wallet = this.users.find(x => x.address === b);
-      return this.send(a, b_wallet, amount);
-    }
-    if (a === b) return;
 
-    const tx = ({
-      from: a.address,
-      to: b.address,
+    this.process({
+      from: alice.address,
+      to: bob.address,
       amount
     });
-
-    this.queue.push(tx);
-    this.events.emit("tx.queued", tx);
   }
 
 }
